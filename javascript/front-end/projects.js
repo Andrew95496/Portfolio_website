04148a2e74a51c6d24dbc47f322a6e2a0423d0e6
Andrew Leacock <andrewleacock96@gmail.com>
2021-12-09 11:01:06 -0500

    tilt.js

---
+++ javascript/front-end/projects.js
@@ -0,0 +1,6 @@
+const hypergraze = document.getElementById("item1");
+const showHypergraze = document.getElementById("graze");
+
+const showItem1 = () => {
+    showHypergraze.classList.toggle("card")
+}
