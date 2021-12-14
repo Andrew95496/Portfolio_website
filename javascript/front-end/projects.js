9c250fd441ea34cd9f9f7e025cac3b25cd8d79d1
Andrew Leacock <andrewleacock96@gmail.com>
2021-12-14 15:42:43 -0500

    added game to project

--- javascript/front-end/projects.js
+++ javascript/front-end/projects.js
@@ -3,67 +3,94 @@ const showKoin = document.getElementById("koin");
 const showElixer = document.getElementById("elixer");
 const showTemp = document.getElementById("temp");
 const showPort = document.getElementById("port");
+const showGame = document.getElementById("game");
 
 
+
+document.addEventListener('dblclick', () =>{
+    showHypergraze.classList.remove("card")
+    showKoin.classList.remove("card")
+    showElixer.classList.remove("card")
+    showTemp.classList.remove("card")
+    showPort.classList.remove("card")
+    showGame.classList.remove("card")
+})
 
 
 
 // hypergraze
 const showItem1 = () => {
-    showHypergraze.classList.toggle("card")
+    showHypergraze.classList.add("card")
 
     // hide others
     showKoin.classList.remove("card")
     showElixer.classList.remove("card")
     showTemp.classList.remove("card")
     showPort.classList.remove("card")
+    showGame.classList.remove("card")
+
 }
 
 
 // katanakoin
 const showItem2 = () => {
-    showKoin.classList.toggle("card")
+    showKoin.classList.add("card")
 
     // hide others
     showHypergraze.classList.remove("card")
     showElixer.classList.remove("card")
     showTemp.classList.remove("card")
     showPort.classList.remove("card")
+    showGame.classList.remove("card")
 }
 
 
 // elixer
 const showItem3 = () => {
-    showElixer.classList.toggle("card")
+    showElixer.classList.add("card")
 
     // hide others
     showKoin.classList.remove("card")
     showHypergraze.classList.remove("card")
     showTemp.classList.remove("card")
     showPort.classList.remove("card")
-
+    showGame.classList.remove("card")
 }
 
 
 // templates
 const showItem4 = () => {
-    showTemp.classList.toggle("card")
+    showTemp.classList.add("card")
 
     // hide others
     showKoin.classList.remove("card")
     showHypergraze.classList.remove("card")
     showElixer.classList.remove("card")
     showPort.classList.remove("card")
+    showGame.classList.remove("card")
 }
 
 
 // porfolio
 const showItem5 = () => {
-    showPort.classList.toggle("card")
+    showPort.classList.add("card")
 
     // hide others
     showKoin.classList.remove("card")
     showHypergraze.classList.remove("card")
     showTemp.classList.remove("card")
     showElixer.classList.remove("card")
+    showGame.classList.remove("card")
+}
+
+// game
+const showItem6 = () => {
+    showGame.classList.add("card")
+
+    // hide others
+    showKoin.classList.remove("card")
+    showHypergraze.classList.remove("card")
+    showTemp.classList.remove("card")
+    showElixer.classList.remove("card")
+    showPort.classList.remove("card")
 }
